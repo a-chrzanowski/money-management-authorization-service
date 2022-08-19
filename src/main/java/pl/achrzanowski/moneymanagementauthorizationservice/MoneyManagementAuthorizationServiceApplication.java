4beb4a0d90a8e3b9a7d0c9d3a15404dc377cd699
aleksander.chrzanowski.dev <aleksander.chrzanowski.dev@gmail.com>
2022-08-19 15:48:18 +0200

    Authorization server for local config implemented

---
+++ src/main/java/pl/achrzanowski/moneymanagementauthorizationservice/MoneyManagementAuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.achrzanowski.moneymanagementauthorizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagementAuthorizationServiceApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(MoneyManagementAuthorizationServiceApplication.class, args);
+    }
+
+}
