12e422b553f35183c985e8275e99465655cdbba6
aleksander.chrzanowski.dev <aleksander.chrzanowski.dev@gmail.com>
2022-08-23 13:55:06 +0200

    Banner changed. Redirect URIs changed

--- src/main/java/pl/achrzanowski/moneymanagementauthorizationservice/config/AuthorizationServerConfig.java
+++ src/main/java/pl/achrzanowski/moneymanagementauthorizationservice/config/AuthorizationServerConfig.java
@@ -42,8 +42,7 @@ public class AuthorizationServerConfig {
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/expense-client-oidc")
-                .redirectUri("http://127.0.0.1:8080/authorized")
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/expense-client-authorization-code")
                 .scope(OidcScopes.OPENID)
                 .scope("expense.read")
                 .build();
