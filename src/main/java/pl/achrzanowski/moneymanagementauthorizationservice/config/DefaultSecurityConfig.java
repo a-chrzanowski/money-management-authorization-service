d97178ff1629f08342f81811ede8fd14568a1936
aleksander.chrzanowski.dev <aleksander.chrzanowski.dev@gmail.com>
2022-11-16 20:50:32 +0100

    Custom user login page added

--- src/main/java/pl/achrzanowski/moneymanagementauthorizationservice/config/DefaultSecurityConfig.java
+++ src/main/java/pl/achrzanowski/moneymanagementauthorizationservice/config/DefaultSecurityConfig.java
@@ -1,22 +1,14 @@
 package pl.achrzanowski.moneymanagementauthorizationservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class DefaultSecurityConfig {
@@ -25,22 +17,25 @@ public class DefaultSecurityConfig {
     private DataSource dataSource;
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .anyRequest().authenticated()
-                )
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
-                .formLogin(withDefaults());
-        return httpSecurity.build();
-    }
-
-    @Bean
     UserDetailsManager userDetailsManager(){
         return new JdbcUserDetailsManager(dataSource);
     }
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .mvcMatchers("/content/**").permitAll()
+                                .anyRequest().authenticated())
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .formLogin(form -> form
+                                .loginPage("/login")
+                                .permitAll());
+        return httpSecurity.build();
+    }
+
 
 }
